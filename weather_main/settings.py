79540a0167e0935ba04221ad79068e957a1a3a5a
Gor <gorsargsyanm@gmail.com>
2020-12-02 02:33:35 +0400

    change settings

--- weather_main/settings.py
+++ weather_main/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 STATIC_URL = '=/staticfiles/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR / 'staticfiles')]
